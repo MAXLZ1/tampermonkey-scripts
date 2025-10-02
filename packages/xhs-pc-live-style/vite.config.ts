2a005c4e9a8956df5c334dd193523e64f9f6e709
MAXLZ <2251683416@qq.com>
2025-10-02 15:40:52 +0800

    fix: cacel code compression

--- packages/xhs-pc-live-style/vite.config.ts
+++ packages/xhs-pc-live-style/vite.config.ts
@@ -3,9 +3,6 @@ import monkey from "vite-plugin-monkey";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    minify: "terser",
-  },
   plugins: [
     monkey({
       entry: "src/main.ts",
